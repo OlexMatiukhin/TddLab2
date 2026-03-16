a60c9515c70c5c95147d9fb9556cd554c7c2ffd0
Olex Matiukhin <aleksksandr3511@gmail.com>
2026-03-16 19:00:38 +0200

    Lab 3 initial commit

--- src/test/java/edu3431/matiukhin/tddlab2/ProductServiceTest.java
+++ src/test/java/edu3431/matiukhin/tddlab2/ProductServiceTest.java
@@ -2,6 +2,7 @@ package edu3431.matiukhin.tddlab2;
 
 
 import edu3431.matiukhin.tddlab2.model.Product;
+import edu3431.matiukhin.tddlab2.repository.ProductRepository;
 import edu3431.matiukhin.tddlab2.request.ProductRequest;
 import edu3431.matiukhin.tddlab2.response.ApiResponse;
 import edu3431.matiukhin.tddlab2.response.BaseMetaData;
@@ -27,11 +28,14 @@ import static org.junit.jupiter.api.Assertions.*;
 @since 16.03.2026 - 16 - 12
 */
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProductServiceTest {
 
    @Autowired
     private ProductService underTest;
+   @Autowired
+           private ProductRepository productRepository;
 
     List<Product> items = new ArrayList<>();
 
@@ -46,17 +50,17 @@ class ProductServiceTest {
     void tearsDown(){
     }
 
-
+   @Order(1)
     @Test
     void whenGetAllItemsListThenSizeIs30() {
         int size = underTest.getAll().size();
         assertEquals(30, size);
     }
-
+ @Order(2)
  @Test
  void whenItemIsPresentThenReturnAsOkApiResponse() {
   // given
-  String id = "69b80d08825fdb764db76d21";
+  String id = "69b836a5980f82888e38ed52";
   // when
   ProductRequest item = underTest.getById(id);
   ApiResponse<BaseMetaData, ProductRequest> response = underTest.getByIdAsApiResponse(id);
@@ -69,7 +73,7 @@ class ProductServiceTest {
   assertNull(response.getMeta().getErrorMessage());
   assertEquals(item, response.getData().get(0));
  }
-
+ @Order(3)
  @Test
  void whenItemIsNotPresentThenReturn400ApiResponseCode_404() {
   // given
@@ -85,39 +89,11 @@ class ProductServiceTest {
   assertEquals("Not found",response.getMeta().getErrorMessage());
  }
 
- @Test
- void whenItemsArePresentThenReturnAsOkApiResponse() {
-  // when
-  List<ProductRequest> items = underTest.getAll();
-  ApiResponse<BaseMetaData, ProductRequest> response = underTest.getAllAsApiResponse();
-
-  // then
-  assertNotNull(response);
-  assertNotNull(response.getData());
-  assertFalse(response.getData().isEmpty());
-  assertNotNull(response.getData().get(0));
-  assertTrue(response.getMeta().isSuccess());
-  assertEquals(200, response.getMeta().getCode());
-  assertNull(response.getMeta().getErrorMessage());
-  assertEquals(items, response.getData());
- }
-
- @Test
- void whenItemsAreNotPresentThenReturnOkApiResponseWithEmptyData() {
-  // when
-  ApiResponse<BaseMetaData, ProductRequest> response = underTest.getAllAsApiResponse();
-  // then
-  assertNotNull(response);
-  assertNotNull(response.getData());
-  assertTrue(response.getData().isEmpty());
-  assertTrue(response.getMeta().isSuccess());
-  assertEquals(200, response.getMeta().getCode());
-  assertNull(response.getMeta().getErrorMessage());
- }
+ @Order(4)
  @Test
  void whenItemIsPresentThenReturnUpdatedItemAsOkApiResponse() {
   // given
-  String id = "69b80d08825fdb764db76d21";
+  String id = "69b836a5980f82888e38ed52";
   ProductRequest request = new ProductRequest(
           id,
           "Updated category",
@@ -141,6 +117,7 @@ class ProductServiceTest {
   assertNull(response.getMeta().getErrorMessage());
   assertEquals(request, response.getData().get(0));
  }
+ @Order(5)
  @Test
  void whenItemIsNotPresentThenReturn400ApiResponseCode404() {
   // given
@@ -166,6 +143,37 @@ class ProductServiceTest {
   assertEquals("Not found", response.getMeta().getErrorMessage());
  }
 
+ @Order(6)
+ @Test
+ void whenItemsArePresentThenReturnAsOkApiResponse() {
+  // when
+  List<ProductRequest> items = underTest.getAll();
+  ApiResponse<BaseMetaData, ProductRequest> response = underTest.getAllAsApiResponse();
+
+  // then
+  assertNotNull(response);
+  assertNotNull(response.getData());
+  assertFalse(response.getData().isEmpty());
+  assertNotNull(response.getData().get(0));
+  assertTrue(response.getMeta().isSuccess());
+  assertEquals(200, response.getMeta().getCode());
+  assertNull(response.getMeta().getErrorMessage());
+  assertEquals(items, response.getData());
+ }
+ @Order(7)
+ @Test
+ void whenItemsAreNotPresentThenReturnOkApiResponseWithEmptyData() {
+  productRepository.deleteAll();
+  // when
+  ApiResponse<BaseMetaData, ProductRequest> response = underTest.getAllAsApiResponse();
+  // then
+  assertNotNull(response);
+  assertNotNull(response.getData());
+  assertTrue(response.getData().isEmpty());
+  assertTrue(response.getMeta().isSuccess());
+  assertEquals(200, response.getMeta().getCode());
+  assertNull(response.getMeta().getErrorMessage());
+ }
 
 
 
